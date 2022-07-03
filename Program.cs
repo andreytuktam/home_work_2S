c8af8156b42c44e4dc22ece132f5f2d15429b180
Andrey Tuktam <tuktam@mail.ru>
2022-07-04 00:29:26 +0700

    03/07/2022

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+int firstthree(int start, int finish){
+int numberA = new Random().Next(start, finish);
+Console.WriteLine(numberA);
+int firstname = numberA / 100;
+//int secondname = (numberA % 100) / 10;
+int threename = numberA % 10;
+int result = firstname*10 + threename;
+return result;
+}
+
+int A = firstthree(100, 999);
+Console.Write(A);
+
+
+int numberA = new Random().Next(1, 100 );
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+    if (numberA >= numberB){
+        if (numberA % numberB == 0){
+            Console.WriteLine(numberA + " кратное " + numberB);
+        } else{
+            Console.WriteLine(numberA % numberB + " - остаток");
+        }
+    }
+    if (numberB >= numberA){
+        if (numberB % numberA == 0){
+            Console.WriteLine(numberB + " кратное " + numberA);
+        } else{
+            Console.WriteLine(numberB % numberA + " - остаток");
+        }
+    }
+
+
+
+int mass = Convert.ToInt32(Console.ReadLine());
+int [] arrey = new int [mass];
+for (int i = 0; i < mass; i++){
+    arrey[i] = i;
+    Console.WriteLine(arrey[i]);
+}
+*/
+
