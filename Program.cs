c55af6de3ed496f6f651934658095efa523580cf
Andrey Tuktam <tuktam@mail.ru>
2022-07-05 13:20:13 +0700

    Z_10, Z_13, Z_15

--- Program.cs
+++ Program.cs
@@ -40,5 +40,71 @@ for (int i = 0; i < mass; i++){
     arrey[i] = i;
     Console.WriteLine(arrey[i]);
 }
+
+
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+
+
+int numberA = new Random().Next(100, 999);
+Console.WriteLine(numberA);
+int secondname = (numberA % 100) / 10;
+
+Console.Write(secondname);
+
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+
+int numberA = new Random().Next(1, 10000);
+Console.WriteLine(numberA);
+string numberB = numberA.ToString();
+
+int dlina = numberB.Length;
+//Console.WriteLine(dlina);
+
+int n = 0;
+
+if (dlina == 3)
+{
+    Console.WriteLine(numberA % 10 + " - третья цифра");  
+} 
+if (dlina - 3 > n)
+{
+    n = dlina - 3;
+   
+    Console.WriteLine((numberA % (Math.Pow(10, n + 1))) / (Math.Pow(10, n)) + " - третья цифра");   
+} 
+if (dlina < 3) 
+{
+    Console.WriteLine("Третья цифра не обнаружена!");
+}
+
+
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+
 */
+int numberA = new Random().Next(1, 7);
+Console.WriteLine(numberA);
+if (numberA >= 6)
+{
+    Console.WriteLine(numberA + " - выходной день недели");
+}
+else
+{
+    Console.WriteLine(numberA + " - рабочий день недели");
+}
+
 
